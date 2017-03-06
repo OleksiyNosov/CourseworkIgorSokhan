f0569fcb6fcfc2e44c504a62f02ecf4c90353a57
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-03-06 23:31:42 +0200

    Created class SmartPoint

---
+++ CourseworkIgorSokhan/GeometryLibrary/SmartPoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary
+{
+    public class SmartPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public SmartPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public SmartPoint() : this(0, 0) { }
+
+
+        public override string ToString()
+        {
+            return $"({X}:{Y})";
+        }
+
+        public static SmartPoint ConvertFrom(string x, string y)
+        {
+            return new SmartPoint(
+                Convert.ToDouble(x),
+                Convert.ToDouble(y));
+        }
+    }
+}
