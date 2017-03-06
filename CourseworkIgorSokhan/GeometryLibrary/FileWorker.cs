6a373b96b2e15e20f7ce7e3ad5f48460ec1d952f
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-03-06 23:35:47 +0200

    Created class FileWorker

---
+++ CourseworkIgorSokhan/GeometryLibrary/FileWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary
+{
+    public class FileWorker
+    {
+        public static IEnumerable<Triangle> Read(string file)
+        {
+            using (var reader = File.OpenText(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var data = line.Split();
+
+                    if (data.Length == 6)
+                        yield return Triangle.CreateFrom(data);
+                }
+            }
+        }
+    }
+}
