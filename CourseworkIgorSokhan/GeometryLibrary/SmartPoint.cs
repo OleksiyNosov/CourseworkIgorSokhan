899425c2ff30051f712037af498fa1001ea1aa68
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-03-09 23:50:34 +0200

    Added property for calculation vector length

--- CourseworkIgorSokhan/GeometryLibrary/SmartPoint.cs
+++ CourseworkIgorSokhan/GeometryLibrary/SmartPoint.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GeometryLibrary
 {
     public class SmartPoint
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        private Point point;
+
+        public double X
+        {
+            get => point.X;
+            set => point.X = value;
+        }
+        public double Y
+        {
+            get => point.Y;
+            set => point.Y = value;
+        }
+
+        public double Length
+        {
+            get => Math.Sqrt(X * X + Y * Y); 
+        }
 
         public SmartPoint(double x, double y)
         {
@@ -18,17 +34,22 @@ namespace GeometryLibrary
         }
         public SmartPoint() : this(0, 0) { }
 
+        public double LengthTo(SmartPoint smartPoint) 
+            => new SmartPoint(
+                smartPoint.X - X, 
+                smartPoint.Y - Y)
+                    .Length;
 
         public override string ToString()
-        {
-            return $"({X}:{Y})";
-        }
+            => $"({X}:{Y})";
+
+        public Point ToPoint()
+            => point;
 
         public static SmartPoint ConvertFrom(string x, string y)
-        {
-            return new SmartPoint(
+            => new SmartPoint(
                 Convert.ToDouble(x),
                 Convert.ToDouble(y));
-        }
+        
     }
 }
