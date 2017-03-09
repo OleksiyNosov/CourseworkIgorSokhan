21f83ab8f4e3be0cda20172b30ef8a3701a3db60
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-03-09 23:51:55 +0200

    Added IsIsosceles property

--- CourseworkIgorSokhan/GeometryLibrary/Triangle.cs
+++ CourseworkIgorSokhan/GeometryLibrary/Triangle.cs
@@ -16,26 +16,31 @@ namespace GeometryLibrary
             {
                 var edges = new double[]
                 {
-
+                    points[0].LengthTo(points[1]),
+                    points[1].LengthTo(points[2]),
+                    points[2].LengthTo(points[0])
                 };
-                return false;
+
+                return (edges[0] == edges[1]) || (edges[0] == edges[2]);
             }
         }
 
         public static Triangle CreateFrom(string[] data)
-        {
-            return new Triangle(
+            => new Triangle(
                 SmartPoint.ConvertFrom(data[0], data[1]),
                 SmartPoint.ConvertFrom(data[2], data[3]),
                 SmartPoint.ConvertFrom(data[4], data[5]));
-        }
+        
 
         public Triangle(SmartPoint point1, SmartPoint point2, SmartPoint point3)
-        {
-            points = new SmartPoint[] { point1, point2, point3 };
-        }
+            => points = new SmartPoint[] { point1, point2, point3 };
+        
         public Triangle() : this(new SmartPoint(), new SmartPoint(), new SmartPoint()) { }
 
+        public bool IsIntersectsWith(Triangle triangle)
+        {
+            throw new NotImplementedException();
+        }
 
         public override string ToString()
         {
@@ -48,8 +53,9 @@ namespace GeometryLibrary
                 sb.Append(p.ToString());
                 sb.Append(' ');
             }
-
             sb.Append("\n\r");
+
+            sb.AppendLine($"{nameof(IsIsosceles)}: {IsIsosceles}");
 
             return sb.ToString();
         }
